0573ffb0d00e6d0ac15c22967d0d2d2d4fe52aa8
Hugh <hugh.lpd@gmail.com>
2022-07-30 23:29:12 +0800

    init repo

---
+++ app/src/main/java/com/github/hughnew/autolighter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.hughnew.autolighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
